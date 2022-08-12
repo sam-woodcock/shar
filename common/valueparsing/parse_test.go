f1d61dfe7d139b4d498637fd7d6df940152ed30b
Andrei Baker <andreib@crystalconstruct.com>
2022-08-12 11:20:44 +0100

    Fixed parser

--- common/valueparsing/parse_test.go
+++ common/valueparsing/parse_test.go
@@ -1,25 +1,24 @@
 package valueparsing
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestExtract(t *testing.T) {
-	test_text := `"orderId":int(78)`
-	key, _type, value := extract(test_text)
+	text := `"orderId":int(78)`
+	key, varType, value, err := extract(text)
+	require.NoError(t, err)
 	assert.Equal(t, "orderId", key, "Parser returned wrong value of the key")
-	assert.Equal(t, "int", _type, "Parser returned wrong value of the type")
+	assert.Equal(t, "int", varType, "Parser returned wrong value of the type")
 	assert.Equal(t, "78", value, "Parser returned wrong value")
 }
 
 func TestParse(t *testing.T) {
-	test_args := []string{`"orderId":int(78)`, `"height":float64(103.101)`}
-	vars, err := Parse(test_args)
-	if err != nil {
-		t.Fail()
-	}
-
+	args := []string{`"orderId":int(78)`, `"height":float64(103.101)`}
+	vars, err := Parse(args)
+	require.NoError(t, err)
 	expectedOrderId := int(78)
 	expectedHeight := float64(103.101)
 	assert.Equal(t, expectedOrderId, (*vars)["orderId"], "Parser returned wrong value")
