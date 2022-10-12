901782bd1ffb1a43f4d90069f9ce9f9d4d973cfa
Andrei Baker <andreib@crystalconstruct.com>
2022-10-12 01:09:10 +0100

    add boundary timers

--- server/errors/errors.go
+++ server/errors/errors.go
@@ -9,6 +9,13 @@ var (
 	ErrWorkflowInstanceNotFound = errors.New("workflow instance not found")
 	ErrWorkflowNotFound         = errors.New("workflow not found")
 	ErrElementNotFound          = errors.New("element not found")
+	ErrStateNotFound            = errors.New("state not found")
+	ErrJobNotFound              = errors.New("job not found")
+	ErrFatalBadDuration         = &ErrWorkflowFatal{Err: errors.New("timer embargo value could not be evaluated to an int or a duration")}
+)
+
+var (
+	NatsMsgKeyNotFound = "nats: key not found"
 )
 
 // ErrWorkflowFatal signifys that the workflow must terniate
