ba9bc75e8d4ee005de686eb310300c7a48eefc5b
Andrei Baker <andreib@crystalconstruct.com>
2022-11-08 18:37:17 +0000

    fix interfaces

--- cli/flag/flag.go
+++ cli/flag/flag.go
@@ -1,24 +1,34 @@
 package flag
 
 const (
-	CorrelationKey      = "correlationKey"
-	Server              = "server"
-	LogLevel            = "loglevel"
-	Vars				= "vars"
-	VarsShort			= "v"
+	// CorrelationKey is the flag name for the correlation key for a BPMN message.
+	CorrelationKey = "correlationKey"
+	// Server is the fhe flag name for the server address.
+	Server = "server"
+	// LogLevel is the flag name for the logging level.
+	LogLevel = "loglevel"
+	// Vars is the flag name for variables passed to the workflow engine.
+	Vars = "vars"
+	// VarsShort is the flag name for variables passed to the workflow engine.
+	VarsShort = "v"
+	// CorrelationKeyShort is the short flag name for the correlation key for a BPMN message.
 	CorrelationKeyShort = "k"
-	ServerShort         = "s"
-	LogLevelShort       = "l"
-	DebugTrace			= "debug"
-	DebugTraceShort		= "d"
+	// ServerShort is the the short flag name for the server address.
+	ServerShort = "s"
+	// LogLevelShort is the short flag name for the logging level.
+	LogLevelShort = "l"
+	// DebugTrace is the short flag name for the trace switch.
+	DebugTrace = "debug"
+	// DebugTraceShort is the short flag name for the trace switch.
+	DebugTraceShort = "d"
 )
 
 type Set struct {
 	Server         string
 	LogLevel       string
 	CorrelationKey string
-	DebugTrace	   bool
-	Vars		   []string
+	DebugTrace     bool
+	Vars           []string
 }
 
 var Value Set
