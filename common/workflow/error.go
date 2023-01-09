015d4910151d0679f1aad2cbe29dd14724617927
Dan Fletcher <daniel.fletcher@vitrifi.net>
2023-01-09 12:28:12 +0000

    Added comments

--- common/workflow/error.go
+++ common/workflow/error.go
@@ -4,16 +4,18 @@ import (
 	"fmt"
 )
 
-// Error describes a workflow error by code or name
+// WrappedError describes a workflow error by code or name
 type WrappedError interface {
 	Error() string
 }
 
+// Error type for wrapped errors
 type Error struct {
 	Code         string
 	WrappedError error
 }
 
+// Error returns the code and error as a string
 func (e Error) Error() string {
 	var we string
 	if e.WrappedError != nil {
