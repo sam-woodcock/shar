388ea7bc90e40bfe70c2c9dd820b5e3a75ace127
Matthew Brazel <Matthew.Brazel@vitrifi.net>
2023-01-09 17:00:37 +0000

    added a public method to retrieve string type values from vars safely with validation

--- model/element.go
+++ model/element.go
@@ -1,4 +1,21 @@
 package model
 
+import "fmt"
+
 // Vars is a map of variables.  The variables must be primative go types.
 type Vars map[string]any
+
+// vars.Get validates that a key has an underlying string value in the map[string]interface{} vars
+// and safely returns the result
+func (vars Vars) Get(key string) (string, error) {
+	if vars[key] == nil {
+		return "", fmt.Errorf("workflow var %s found nil", key)
+	}
+
+	value, ok := vars[key].(string)
+	if !ok {
+		return "", fmt.Errorf("workflow var %s found non-string type for the underlying value", key)
+	}
+
+	return value, nil
+}
